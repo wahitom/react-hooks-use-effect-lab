b2b9455f39af6b9eae54816f2535fdbaf82880b3
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-12 18:51:24 +0300

    Completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  let timer;
+
+  useEffect(() => {
+    // This block of code runs when the component mounts
+
+    // Set up a timer using setInterval
+    const timer = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    // This is a cleanup function that runs when the component unmounts or when the dependency array changes (in this case, it's an empty dependency array, so it runs only when the component mounts).
+    // It's responsible for clearing the interval to avoid memory leaks.
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      clearInterval(timer);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
